77602cffd48bb2032b14e305407229ed837d5f9e
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-05-21 21:31:45 +0530

    navbar settings

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>OpenWard</h2>
+
+      <div>
+        <a href="#">Home</a>
+        <a href="#">Login</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
