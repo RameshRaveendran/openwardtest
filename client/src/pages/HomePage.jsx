8329a961f0e18e762ec7437d090595a6a6175d5d
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-05-26 15:36:39 +0530

    homepage test

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,25 +2,54 @@ import { useState } from "react";
 
 function HomePage() {
 
-  const [count, setCount] = useState(100);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-  function increase() {
-    setCount(count + "a");
-  }
-  function reset() {
-    setCount(0);
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      title,
+      description,
+    });
   }
 
   return (
     <div>
-      <h1>{count}</h1>
 
-      <button onClick={increase}>
-        Increase
-      </button>
-      <button onClick={reset}>
-        reset 
-      </button>
+      <h1>Create Complaint</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="text"
+          placeholder="Complaint title"
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+
+        <br />
+        <br />
+
+        <textarea
+          placeholder="Complaint description"
+          value={description}
+          onChange={(event) => {
+            setDescription(event.target.value);
+          }}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">
+          Submit Complaint
+        </button>
+
+      </form>
+
     </div>
   );
 }
