d8dc4d91387e4d84777abee90a715685603ecea6
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-05-27 14:51:14 +0530

    app update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,46 @@
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+
 import HomePage from "./pages/HomePage";
-
-
-
+import LoginPage from "./pages/LoginPage";
+import ComplaintPage from "./pages/ComplaintPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
+
   return (
     <div>
-      <Navbar/>
-      <HomePage />
+
+      <Navbar />
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+
+        <Route
+          path="/complaints"
+          element={<ComplaintPage />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<DashboardPage />}
+        />
+
+      </Routes>
+
     </div>
   );
 }
