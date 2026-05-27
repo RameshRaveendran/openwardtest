aeaa91d399c1e46c858daba7a8c66508b87a2f71
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-05-27 14:55:02 +0530

    main layout

---
+++ client/src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+function MainLayout({ children }) {
+
+  return (
+    <div>
+
+      <Navbar />
+
+      {children}
+
+    </div>
+  );
+}
+
+export default MainLayout;
