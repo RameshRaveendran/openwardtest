02bf19a4aa40a3286de6855f33b8e77df94a0c5c
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-05-27 14:52:55 +0530

    navbar updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,48 @@
-function Navbar() {
-  return (
-    <nav>
-      <h2>OpenWard</h2>
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
-      <div>
-        <a href="#">Home</a>
-        <a href="#">Login</a>
-        <a href="#">About</a>
-        <a href="#">Complaints</a>
-      </div>
-    </nav>
+import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ComplaintPage from "./pages/ComplaintPage";
+import DashboardPage from "./pages/DashboardPage";
+
+function App() {
+
+  return (
+    <div>
+
+      <Navbar />
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+
+        <Route
+          path="/complaints"
+          element={<ComplaintPage />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<DashboardPage />}
+        />
+
+      </Routes>
+
+    </div>
   );
 }
 
-export default Navbar;
+export default App;
