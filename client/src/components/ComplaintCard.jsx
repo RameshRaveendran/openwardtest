1a13ecb1782f7dfc0b17a1ae4758ac679827c7c3
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-05-29 15:42:08 +0530

    complaint componenet

---
+++ client/src/components/ComplaintCard.jsx
@@ -0,0 +1,26 @@
+function ComplaintCard() {
+  return (
+    <div
+      className="
+      border
+      rounded-lg
+      p-4
+      mb-4
+      "
+    >
+      <h2 className="font-bold">
+        Broken Street Light
+      </h2>
+
+      <p>
+        Light not working for 3 days.
+      </p>
+
+      <span>
+        Pending
+      </span>
+    </div>
+  );
+}
+
+export default ComplaintCard;
